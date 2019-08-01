8378ae0d12530021471ff859c62e3683919c4372
usc-stl004 <stl004@student.usc.edu.au>
2019-08-01 10:38:52 +1000

    question class

---
+++ app/src/main/java/android/bignerdbranch/com/Question.java
@@ -0,0 +1,28 @@
+package android.bignerdbranch.com;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int question_australia, boolean b) {
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+
+
+}
